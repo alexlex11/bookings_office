ebce8cad475ec60a0d25391e733ae3f2de607769
sh-rh <s@DESKTOP-0LR5JGK.>
2024-09-11 21:38:38 +0300

    +models

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MeetingRoom(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    descriptions = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}: {self.descriptions}'
+
+
+class Booking(models.Model):
+    meeting_room = models.ForeignKey(MeetingRoom, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    descriptions = models.TextField(blank=True, null=True)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    is_free = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} booked a {self.meeting_room} from {self.start_datetime} to {self.end_datetime} for {self.descriptions}.'
